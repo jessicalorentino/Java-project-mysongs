035b377341e8aa99625df5f7083acd90e8711dd7
jessicalorentino <contato.jessicajsl@gmail.com>
2024-06-14 01:48:18 -0300

    Projeto Java

---
+++ src/br/com/mysongs/principal/Principal.java
@@ -0,0 +1,37 @@
+package br.com.mysongs.principal;
+
+import br.com.mysongs.modelos.MinhasPreferidas;
+import br.com.mysongs.modelos.Musica;
+import br.com.mysongs.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Always");
+        minhaMusica.setCantor("Bon Jovi");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("Código fonte TV");
+        meuPodcast.setApresentador("Vanessa Weber e Gabriel Fróes.");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+    }
+}
